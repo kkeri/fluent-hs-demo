fd89e66271d54e97c295deadeca541b851630d46
Kalman Keri <kk@metamaya.org>
2024-05-26 19:11:30 +0200

    split the kernel to parser and interpreter

--- app/Main.hs
+++ app/Main.hs
@@ -8,18 +8,20 @@ import           Core
 import           Handler
 import           Proc
 
--- Build a kernel that interpreters a string.
--- kern (s ++ t) = (kern s) ++ (kern t)
-kern :: String -> Prog Neg Pos
-kern s = [ Neg Vals
-         , Neg Defs
-         , Neg Lists
-         , Neg Tokens
-         , Pos $ PToken (Str s)
-         ]
+-- A program that parses a string into a flat list of terms.
+parser :: String -> Prog Neg Pos
+parser s = [ Neg Lists
+           , Neg Tokens
+           , Pos $ PToken (Str s)
+           ]
+
+-- A program that interpreters a flat list of terms.
+-- kernel (s ++ t) = (kernel s) ++ (kernel t)
+kernel :: Prog Neg Pos
+kernel = [Neg Vals, Neg Defs]
 
 main :: IO ()
 main = do
   s <- getContents
-  xc <- execIO . interactHandler . defHandler [] . cont $ kern s
+  xc <- execIO . interactHandler . defHandler [] . cont $ kernel ++ parser s
   exitWith xc
